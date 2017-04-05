1aac02d893d3a3998e5c4db247016634b789d125
Andrea Ortiz <aortiz1865@gmail.com>
2017-04-05 11:18:51 -0500

    updated layerPrep.R

--- layerPrep.R
+++ layerPrep.R
@@ -33,7 +33,7 @@ dir.create("layers")
 
 ###Past 1930
 ## load PRISM1930 ppt layers
-ppt_4kmM2_0 <- raster("~data/dataLayers/PRISM1930/PRISM_ppt_stable_4kmM2_1930_all_bil/PRISM_ppt_stable_4kmM2_1930_bil.bil")
+ppt_4kmM2_0 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1930/PRISM_ppt_stable_4kmM2_1930_all_bil/PRISM_ppt_stable_4kmM2_1930_bil.bil")
 
 ##change projection of past data layers
 projection(ppt_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -41,10 +41,10 @@ projection(ppt_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,
 ## clip data layers
 ppt_0 <- mask(ppt_4kmM2_0, middleUS)
 ppt.0 <- crop(ppt_0, extent(middleUS))
-writeRaster(ppt.0, "~data/PastLayers/1930/ppt0.asc", format="ascii", overwrite=TRUE)
+writeRaster(ppt.0, "~/Documents/Thesis/data/PastLayers_clipped/1930/ppt0.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM1930 tmax layers
-tmax_4kmM2_0 <- raster("~data/dataLayers/PRISM1930/PRISM_tmax_stable_4kmM2_1930_all_bil/PRISM_tmax_stable_4kmM2_1930_bil.bil")
+tmax_4kmM2_0 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1930/PRISM_tmax_stable_4kmM2_1930_all_bil/PRISM_tmax_stable_4kmM2_1930_bil.bil")
 
 ##change projection of past data layers
 projection(tmax_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -52,10 +52,10 @@ projection(tmax_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 tmax_0 <- mask(tmax_4kmM2_0, middleUS)
 tmax.0 <- crop(tmax_0, extent(middleUS))
-writeRaster(tmax.0, "/Users/hertwecklab/Documents/Thesis/data/PastLayers/1930/tmax0.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmax.0, "~/Documents/Thesis/data/PastLayers_clipped/1930/tmax0.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM1930 tmean layers
-tmean_4kmM2_0 <- raster("/Users/hertwecklab/Documents/Thesis/data/dataLayers/PRISM1930/PRISM_tmean_stable_4kmM2_1930_all_bil/PRISM_tmean_stable_4kmM2_1930_bil.bil")
+tmean_4kmM2_0 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1930/PRISM_tmean_stable_4kmM2_1930_all_bil/PRISM_tmean_stable_4kmM2_1930_bil.bil")
 
 ##change projection of past data layers
 projection(tmean_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -63,10 +63,10 @@ projection(tmean_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,
 ## clip data layers
 tmean_0 <- mask(tmean_4kmM2_0, middleUS)
 tmean.0 <- crop(tmean_0, extent(middleUS))
-writeRaster(tmean.0, "/Users/hertwecklab/Documents/Thesis/data/PastLayers/1930/tmean0.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmean.0, "~/Documents/Thesis/data/PastLayers_clipped/1930/tmean0.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM1930 tmin layers
-tmin_4kmM2_0 <- raster("~data/dataLayers/PRISM1930/PRISM_tmin_stable_4kmM2_1930_all_bil/PRISM_tmin_stable_4kmM2_1930_bil.bil")
+tmin_4kmM2_0 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1930/PRISM_tmin_stable_4kmM2_1930_all_bil/PRISM_tmin_stable_4kmM2_1930_bil.bil")
 
 ##change projection of past data layers
 projection(tmin_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -74,7 +74,7 @@ projection(tmin_4kmM2_0) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 tmin_0 <- mask(tmin_4kmM2_0, middleUS)
 tmin.0 <- crop(tmin_0, extent(middleUS))
-writeRaster(tmin.0, "~data/PastLayers/1930/tmin0.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmin.0, "~/Documents/Thesis/data/PastLayers_clipped/1930/tmin0.asc", format="ascii", overwrite=TRUE)
 
 ## if layers have already been clipped, masked and saved and you need to reload them:
 ppt0 <- raster("layers/avg_1930_ppt0.asc")
@@ -93,7 +93,7 @@ write.csv(c0, "correlation1930.csv")
 
 ###Past 2014
 ## load PRISM2014 ppt layers
-ppt_4kmM3_1 <- raster("~data/dataLayers/PRISM2014/PRISM_ppt_stable_4kmM3_2014_all_bil/PRISM_ppt_stable_4kmM3_2014_bil.bil")
+ppt_4kmM3_1 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2014/PRISM_ppt_stable_4kmM3_2014_all_bil/PRISM_ppt_stable_4kmM3_2014_bil.bil")
 
 ##change projection of past data layers
 projection(ppt_4kmM3_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -101,10 +101,10 @@ projection(ppt_4kmM3_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,
 ## clip data layers
 ppt_1 <- mask(ppt_4kmM3_1, middleUS)
 ppt.1 <- crop(ppt_1, extent(middleUS))
-writeRaster(ppt.1, "~data/PastLayers/2014/ppt1.asc", format="ascii", overwrite=TRUE)
+writeRaster(ppt.1, "~/Documents/Thesis/data/PastLayers_clipped/2014/ppt1.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM2014 tmax layers
-tmax_4kmM2_1 <- raster("~data/dataLayers/PRISM2014/PRISM_tmax_stable_4kmM2_2014_all_bil/PRISM_tmax_stable_4kmM2_2014_bil.bil")
+tmax_4kmM2_1 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2014/PRISM_tmax_stable_4kmM2_2014_all_bil/PRISM_tmax_stable_4kmM2_2014_bil.bil")
 
 ##change projection of past data layers
 projection(tmax_4kmM2_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -112,10 +112,10 @@ projection(tmax_4kmM2_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 tmax_1 <- mask(tmax_4kmM2_1, middleUS)
 tmax.1 <- crop(tmax_1, extent(middleUS))
-writeRaster(tmax.1, "~data/PastLayers/2014/tmax1.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmax.1, "~/Documents/Thesis/data/PastLayers_clipped/2014/tmax1.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM2014 tmean layers
-tmean_4kmM2_1 <- raster("~data/dataLayers/PRISM2014/PRISM_tmean_stable_4kmM2_2014_all_bil/PRISM_tmean_stable_4kmM2_2014_bil.bil")
+tmean_4kmM2_1 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2014/PRISM_tmean_stable_4kmM2_2014_all_bil/PRISM_tmean_stable_4kmM2_2014_bil.bil")
 
 ##change projection of past data layers
 projection(tmean_4kmM2_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -123,10 +123,10 @@ projection(tmean_4kmM2_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,
 ## clip data layers
 tmean_1 <- mask(tmean_4kmM2_1, middleUS)
 tmean.1 <- crop(tmean_1, extent(middleUS))
-writeRaster(tmean.1, "~data/PastLayers/2014/tmean1.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmean.1, "~/Documents/Thesis/data/PastLayers_clipped/2014/tmean1.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM2014 tmin layers
-tmin_4kmM2_1 <- raster("~data/dataLayers/PRISM2014/PRISM_tmin_stable_4kmM2_2014_all_bil/PRISM_tmin_stable_4kmM2_2014_bil.bil")
+tmin_4kmM2_1 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2014/PRISM_tmin_stable_4kmM2_2014_all_bil/PRISM_tmin_stable_4kmM2_2014_bil.bil")
 
 ##change projection of past data layers
 projection(tmin_4kmM2_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -134,7 +134,7 @@ projection(tmin_4kmM2_1) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 tmin_1 <- mask(tmin_4kmM2_1, middleUS)
 tmin.1 <- crop(tmin_1, extent(middleUS))
-writeRaster(tmin.1, "~data/PastLayers/2014/tmin1.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmin.1, "~/Documents/Thesis/data/PastLayers_clipped/2014/tmin1.asc", format="ascii", overwrite=TRUE)
 
 ## if layers have already been clipped, masked and saved and you need to reload them:
 ppt1 <- raster("layers/avg_2014_ppt0.asc")
@@ -153,7 +153,7 @@ write.csv(c1, "correlation2014.csv")
 
 ###Past 1929
 ## load PRISM1929 ppt layers
-ppt_4kmM2_9 <- raster("~data/dataLayers/PRISM1929/PRISM_ppt_stable_4kmM2_1929_all_bil/PRISM_ppt_stable_4kmM2_1929_bil.bil")
+ppt_4kmM2_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_ppt_stable_4kmM2_1929_all_bil/PRISM_ppt_stable_4kmM2_1929_bil.bil")
 
 ##change projection of past data layers
 projection(ppt_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -161,10 +161,10 @@ projection(ppt_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,
 ## clip data layers
 ppt_9 <- mask(ppt_4kmM2_9, middleUS)
 ppt.9 <- crop(ppt_9, extent(middleUS))
-writeRaster(ppt.9, "~data/PastLayers/1929/ppt9.asc", format="ascii", overwrite=TRUE)
+writeRaster(ppt.9, "~/Desktop/Allium_nicheModeling/layers/ppt9.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM1929 tmax layers
-tmax_4kmM2_9 <- raster("~data/dataLayers/PRISM1929/PRISM_tmax_stable_4kmM2_1929_all_bil/PRISM_tmax_stable_4kmM2_1929_bil.bil")
+tmax_4kmM2_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_tmax_stable_4kmM2_1929_all_bil/PRISM_tmax_stable_4kmM2_1929_bil.bil")
 
 ##change projection of past data layers
 projection(tmax_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -172,10 +172,10 @@ projection(tmax_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 tmax_9 <- mask(tmax_4kmM2_9, middleUS)
 tmax.9 <- crop(tmax_9, extent(middleUS))
-writeRaster(tmax.9, "/Users/hertwecklab/Documents/Thesis/data/PastLayers/1929/tmax9.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmax.9, "~/Desktop/Allium_nicheModeling/layers/tmax9.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM1929 tmean layers
-tmean_4kmM2_9 <- raster("/Users/hertwecklab/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_tmean_stable_4kmM2_1929_all_bil/PRISM_tmean_stable_4kmM2_1929_bil.bil")
+tmean_4kmM2_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_tmean_stable_4kmM2_1929_all_bil/PRISM_tmean_stable_4kmM2_1929_bil.bil")
 
 ##change projection of past data layers
 projection(tmean_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -183,10 +183,10 @@ projection(tmean_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,
 ## clip data layers
 tmean_9 <- mask(tmean_4kmM2_9, middleUS)
 tmean.9 <- crop(tmean_9, extent(middleUS))
-writeRaster(tmean.9, "/Users/hertwecklab/Documents/Thesis/data/PastLayers/1929/tmean9.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmean.9, "~/Desktop/Allium_nicheModeling/layers/tmean9.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM1929 tmin layers
-tmin_4kmM2_9 <- raster("~data/dataLayers/PRISM1929/PRISM_tmin_stable_4kmM2_1929_all_bil/PRISM_tmin_stable_4kmM2_1929_bil.bil")
+tmin_4kmM2_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_tmin_stable_4kmM2_1929_all_bil/PRISM_tmin_stable_4kmM2_1929_bil.bil")
 
 ##change projection of past data layers
 projection(tmin_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -194,16 +194,52 @@ projection(tmin_4kmM2_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 tmin_9 <- mask(tmin_4kmM2_9, middleUS)
 tmin.9 <- crop(tmin_9, extent(middleUS))
-writeRaster(tmin.9, "~data/PastLayers/1929/tmin9.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmin.9, "~/Desktop/Allium_nicheModeling/layers/tmin9.asc", format="ascii", overwrite=TRUE)
+
+## load PRISM1929 vpdmax layers
+vpdmax_4kmM1_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_vpdmax_stable_4kmM1_1929_all_bil/PRISM_vpdmax_stable_4kmM1_1929_bil.bil")
+
+##change projection of past data layers
+projection(vpdmax_4kmM1_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
+
+## clip data layers
+vpdmax_9 <- mask(vpdmax_4kmM1_9, middleUS)
+vpdmax.9 <- crop(vpdmax_9, extent(middleUS))
+writeRaster(vpdmax.9, "~/Desktop/Allium_nicheModeling/layers/vpdmax9.asc", format="ascii", overwrite=TRUE)
+
+## load PRISM1929 vpdmin layers
+vpdmin_4kmM1_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_vpdmin_stable_4kmM1_1929_all_bil/PRISM_vpdmin_stable_4kmM1_1929_bil.bil")
+
+##change projection of past data layers
+projection(vpdmin_4kmM1_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
+
+## clip data layers
+vpdmin_9 <- mask(vpdmin_4kmM1_9, middleUS)
+vpdmin.9 <- crop(vpdmin_9, extent(middleUS))
+writeRaster(vpdmin.9, "~/Desktop/Allium_nicheModeling/layers/vpdmin9.asc", format="ascii", overwrite=TRUE)
+
+## load PRISM1929 tdmean layers
+tdmean_4kmM1_9 <- raster("~/Documents/Thesis/data/dataLayers/PRISM1929/PRISM_tdmean_stable_4kmM1_1929_all_bil/PRISM_tdmean_stable_4kmM1_1929_bil.bil")
+
+##change projection of past data layers
+projection(tdmean_4kmM1_9) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
+
+## clip data layers
+tdmean_9 <- mask(tdmean_4kmM1_9, middleUS)
+tdmean.9 <- crop(tdmean_9, extent(middleUS))
+writeRaster(tdmean.9, "~/Desktop/Allium_nicheModeling/layers/tdmean9.asc", format="ascii", overwrite=TRUE)
 
 ## if layers have already been clipped, masked and saved and you need to reload them:
-ppt9 <- raster("layers/avg_1929_ppt9.asc")
-tmax9 <- raster("layers/avg_1929_tmax9.asc")
-tmean9 <- raster("layers/avg_1929_tmean9.asc")
-tmin9 <- raster("layers/avg_1929_tmin9.asc")
+ppt9 <- raster("layers/ppt9.asc")
+tmax9 <- raster("layers/tmax9.asc")
+tmean9 <- raster("layers/tmean9.asc")
+tmin9 <- raster("layers/tmin9.asc")
+vpdmax9 <- raster("layers/vpdmax9.asc")
+vpdmin9 <- raster("layers/vpdmin9.asc")
+tdmean9 <- raster("layers/tdmean9.asc")
 
 ## correlation analysis
-stack9 <- stack(tmin9, tmean9, tmax9, ppt9) 
+stack9 <- stack(tmin9, tmean9, tmax9, ppt9, vpdmax9, vpdmin9, tdmean9)  
 corr9 <- layerStats(stack9, 'pearson', na.rm=TRUE)
 c9 <- corr9$`pearson correlation coefficient`
 write.csv(c9, "correlation1929.csv")
@@ -213,7 +249,7 @@ write.csv(c9, "correlation1929.csv")
 
 ###Past 2011
 ## load PRISM2011 ppt layers
-ppt_4kmM3_11 <- raster("~data/dataLayers/PRISM2011/PRISM_ppt_stable_4kmM3_2011_all_bil/PRISM_ppt_stable_4kmM3_2011_bil.bil")
+ppt_4kmM3_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_ppt_stable_4kmM3_2011_all_bil/PRISM_ppt_stable_4kmM3_2011_bil.bil")
 
 ##change projection of past data layers
 projection(ppt_4kmM3_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -221,10 +257,10 @@ projection(ppt_4kmM3_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0
 ## clip data layers
 ppt_11 <- mask(ppt_4kmM3_11, middleUS)
 ppt.11 <- crop(ppt_11, extent(middleUS))
-writeRaster(ppt.11, "~data/PastLayers/2011/ppt11.asc", format="ascii", overwrite=TRUE)
+writeRaster(ppt.11, "~/Desktop/Allium_nicheModeling/layers/ppt11.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM2011 tmax layers
-tmax_4kmM2_11 <- raster("~data/dataLayers/PRISM2011/PRISM_tmax_stable_4kmM2_2011_all_bil/PRISM_tmax_stable_4kmM2_2011_bil.bil")
+tmax_4kmM2_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_tmax_stable_4kmM2_2011_all_bil/PRISM_tmax_stable_4kmM2_2011_bil.bil")
 
 ##change projection of past data layers
 projection(tmax_4kmM2_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -232,10 +268,10 @@ projection(tmax_4kmM2_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,
 ## clip data layers
 tmax_11 <- mask(tmax_4kmM2_11, middleUS)
 tmax.11 <- crop(tmax_11, extent(middleUS))
-writeRaster(tmax.11, "~data/PastLayers/2011/tmax11.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmax.11, "~/Desktop/Allium_nicheModeling/layers/tmax11.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM2011 tmean layers
-tmean_4kmM2_11 <- raster("~data/dataLayers/PRISM2011/PRISM_tmean_stable_4kmM2_2011_all_bil/PRISM_tmean_stable_4kmM2_2011_bil.bil")
+tmean_4kmM2_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_tmean_stable_4kmM2_2011_all_bil/PRISM_tmean_stable_4kmM2_2011_bil.bil")
 
 ##change projection of past data layers
 projection(tmean_4kmM2_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -243,10 +279,10 @@ projection(tmean_4kmM2_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0
 ## clip data layers
 tmean_11 <- mask(tmean_4kmM2_11, middleUS)
 tmean.11 <- crop(tmean_11, extent(middleUS))
-writeRaster(tmean.11, "~data/PastLayers/2011/tmean11.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmean.11, "~/Desktop/Allium_nicheModeling/layers/tmean11.asc", format="ascii", overwrite=TRUE)
 
 ## load PRISM2011 tmin layers
-tmin_4kmM2_11 <- raster("~data/dataLayers/PRISM2011/PRISM_tmin_stable_4kmM2_2011_all_bil/PRISM_tmin_stable_4kmM2_2011_bil.bil")
+tmin_4kmM2_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_tmin_stable_4kmM2_2011_all_bil/PRISM_tmin_stable_4kmM2_2011_bil.bil")
 
 ##change projection of past data layers
 projection(tmin_4kmM2_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
@@ -254,16 +290,52 @@ projection(tmin_4kmM2_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,
 ## clip data layers
 tmin_11 <- mask(tmin_4kmM2_11, middleUS)
 tmin.11 <- crop(tmin_11, extent(middleUS))
-writeRaster(tmin.11, "~data/PastLayers/2011/tmin11.asc", format="ascii", overwrite=TRUE)
+writeRaster(tmin.11, "~/Desktop/Allium_nicheModeling/layers/tmin11.asc", format="ascii", overwrite=TRUE)
+
+## load PRISM2011 vpdmax layers
+vpdmax_4kmM1_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_vpdmax_stable_4kmM1_2011_all_bil/PRISM_vpdmax_stable_4kmM1_2011_bil.bil")
+
+##change projection of past data layers
+projection(vpdmax_4kmM1_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
+
+## clip data layers
+vpdmax_11 <- mask(vpdmax_4kmM1_11, middleUS)
+vpdmax.11 <- crop(vpdmax_11, extent(middleUS))
+writeRaster(vpdmax.11, "~/Desktop/Allium_nicheModeling/layers/vpdmax11.asc", format="ascii", overwrite=TRUE)
+
+## load PRISM2011 vpdmin layers
+vpdmin_4kmM1_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_vpdmin_stable_4kmM1_2011_all_bil/PRISM_vpdmin_stable_4kmM1_2011_bil.bil")
+
+##change projection of past data layers
+projection(vpdmin_4kmM1_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
+
+## clip data layers
+vpdmin_11 <- mask(vpdmin_4kmM1_11, middleUS)
+vpdmin.11 <- crop(vpdmin_11, extent(middleUS))
+writeRaster(vpdmin.11, "~/Desktop/Allium_nicheModeling/layers/vpdmin11.asc", format="ascii", overwrite=TRUE)
+
+## load PRISM2011 tdmean layers
+tdmean_4kmM1_11 <- raster("~/Documents/Thesis/data/dataLayers/PRISM2011/PRISM_tdmean_stable_4kmM1_2011_all_bil/PRISM_tdmean_stable_4kmM1_2011_bil.bil")
+
+##change projection of past data layers
+projection(tdmean_4kmM1_11) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs") #project
+
+## clip data layers
+tdmean_11 <- mask(tdmean_4kmM1_11, middleUS)
+tdmean.11 <- crop(tdmean_11, extent(middleUS))
+writeRaster(tdmean.11, "~/Desktop/Allium_nicheModeling/layers/tdmean11.asc", format="ascii", overwrite=TRUE)
 
 ## if layers have already been clipped, masked and saved and you need to reload them:
-ppt11 <- raster("layers/avg_2011_ppt11.asc")
-tmax11 <- raster("layers/avg_2011_tmax11.asc")
-tmean1 <- raster("layers/avg_2011_tmean11.asc")
-tmin11 <- raster("layers/avg_2011_tmin11.asc")
+ppt11 <- raster("layers/ppt11.asc")
+tmax11 <- raster("layers/tmax11.asc")
+tmean11 <- raster("layers/tmean11.asc")
+tmin11 <- raster("layers/tmin11.asc")
+vpdmax11 <- raster("layers/vpdmax11.asc")
+vpdmin11 <- raster("layers/vpdmin11.asc")
+tdmean11 <- raster("layers/tdmean11.asc")
 
 ## correlation analysis
-stack11 <- stack(tmin11, tmean11, tmax11, ppt11) 
+stack11 <- stack(tmin11, tmean11, tmax11, ppt11, vpdmax11, vpdmin11, tdmean11) 
 corr11 <- layerStats(stack11, 'pearson', na.rm=TRUE)
 c11 <- corr11$`pearson correlation coefficient`
 write.csv(c11, "correlation2011.csv")
