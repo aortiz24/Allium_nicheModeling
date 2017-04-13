52d5f79419284ae7961e5a064112911670ff9a3e
Andrea Ortiz <aortiz1865@gmail.com>
2017-04-13 18:28:52 -0500

    updated correlation criteria

--- layerPrep.R
+++ layerPrep.R
@@ -125,7 +125,7 @@ c9 <- corr9$`pearson correlation coefficient`
 write.csv(c9, "correlation1929.csv")
 # inspect output for correlations between layers
 # absolute value of 0.7 or greater are correlated
-#   for this analysis, retain tmean9, ppt9, vpdmax9
+# for this analysis, retain tmean9, ppt9, vpdmax9
 
 ###Past 2011
 ## load PRISM2011 ppt layers
@@ -220,5 +220,5 @@ corr11 <- layerStats(stack11, 'pearson', na.rm=TRUE)
 c11 <- corr11$`pearson correlation coefficient`
 write.csv(c11, "correlation2011.csv")
 # inspect output for correlations between layers
-#   0.7 and above (or -0.7 and below) are correlated
-#   for this analysis, retain tmean11, ppt11, vpdmin11, tdmean11
+# absolute value of 0.7 or greater are correlated
+# for this analysis, retain tmean11, ppt11, vpdmin11, tdmean11
